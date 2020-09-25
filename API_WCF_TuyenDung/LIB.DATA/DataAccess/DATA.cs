7f1958dafa4b914811af5ae5d1f951f77532fc0d
TuanNghia <truongtuannghia328@gmail.com>
2020-09-25 11:53:43 +0700

    Tạo class Data để truyền nhận tham số store procedurce

---
+++ API_WCF_TuyenDung/LIB.DATA/DataAccess/DATA.cs
@@ -0,0 +1,13 @@
+﻿using LIB.DATA.IDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIB.DATA.DataAccess
+{
+    class DATA : IDATA
+    {
+    }
+}
