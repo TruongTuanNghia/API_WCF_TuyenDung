d6cf6bf9c406387f0b5a311729970f6aab91a9fc
TuanNghia <truongtuannghia328@gmail.com>
2020-09-28 13:21:33 +0700

    cập nhật api cập nhật kinh nghiệm

--- API_WCF_TuyenDung/API_WCF_TuyenDung/Request.cs
+++ API_WCF_TuyenDung/API_WCF_TuyenDung/Request.cs
@@ -8,8 +8,8 @@ namespace API_WCF_TuyenDung
     
         public class LOGIN_INPUT
         {
-            public string userNam { get; set; }
-            public string passWord { get; set; }
+            public string userName { get; set; }
+            public string userPassword { get; set; }
             public int? statusLG { get; set; }
             public int typeLogin { get; set; }
          }
@@ -29,13 +29,13 @@ namespace API_WCF_TuyenDung
     public class INSERT_EXPERIENCES_INPUT
     {
         public int idExperiences { get; set; }
-        public int idUser { get; set; }
-        public string titleJob { get; set; }
-        public string nameCompanny { get; set; }
-        public string startDay { get; set; }
-        public string endDay { get; set; }
-        public bool statusDoingJob { get; set; }
-        public string note { get; set; }
+        public int IdUser { get; set; }
+        public string Title { get; set; }
+        public string NameCompanny { get; set; }
+        public string StartDay { get; set; }
+        public string EndDay { get; set; }
+        public bool? StatusDoingJob { get; set; }
+        public string Note { get; set; }
     }
 
     public class DELETE_EXPERIENCES_INPUT
